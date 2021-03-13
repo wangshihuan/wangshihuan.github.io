0fedce1cd93cb7a1c27aab5fdd30ef9bcbab109f
Shihuan Wang <441300774@qq.com>
2021-03-13 11:15:36 +0800

    Update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 
-  $('a.blog-button').click(function() {
+  /*$('a.blog-button').click(function() {
     // If already in blog, return early without animate overlay panel again.
     if (location.hash && location.hash == "#blog") return;
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
@@ -15,7 +15,7 @@ $(document).ready(function() {
     }
 
     
-  });
+  }*/);
 
   if (window.location.hash && window.location.hash == "#blog") {
     $('.panel-cover').addClass('panel-cover--collapsed');
